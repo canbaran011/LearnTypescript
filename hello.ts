271cbfe4243c45f2fff19b7facb7ad456acbf263
Can Baran Gündüz <canbaran011@gmail.com>
2021-05-22 12:23:37 +0300

    first add

---
+++ hello.ts
@@ -0,0 +1,26 @@
+let helloWorld = "Hello World";
+console.log(helloWorld);
+
+interface User{
+    name:string;
+    id:number;
+}
+
+const user = {
+    name: "Hayes",
+    id: 0,
+  };
+  console.log(user.name)
+  console.log(typeof(user.id))
+
+const user2 : User= {
+    name: "Can Baran",
+    id: 1,
+};
+
+console.log(user2.name)
+
+
+
+
+
